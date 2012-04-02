6487de6af226ffe78435e0a2fef460a5a35cb55d
François Kooman <fkooman@tuxed.net>
2012-04-02 16:57:21 +0200

    use local jquery/jsrender and update example client to use jsrender

--- docs/install.sh
+++ docs/install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+
+# create directories and set permissions
 mkdir -p data
+mkdir -p ext
+mkdir -p ext/js
 docs/reset_oauth.sh
 docs/reset_voot.sh
 chmod -R o+w data/
 chcon -R -t httpd_sys_rw_content_t data
+
+# slim
 if [ ! -d ext/Slim ]
 then
-	mkdir -p ext/
 	cd ext/
 	git clone git://github.com/codeguy/Slim.git
 else
@@ -14,3 +19,18 @@ else
 	git pull
 fi
 
+####################################
+# Dependencies for the demo client #
+####################################
+
+# fetch jquery
+if [ ! -f ext/js/jquery.js ]
+then
+    wget -O ext/js/jquery.js -N http://code.jquery.com/jquery.min.js
+fi
+
+# fetch jsrender
+if [ ! -f ext/js/jsrender.js ]
+then
+    wget -O ext/js/jsrender.js -N https://raw.github.com/BorisMoore/jsrender/master/jsrender.js
+fi
