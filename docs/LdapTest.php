fdbd1da7901cb8ab10d524d81e946bba6dad5c83
François Kooman <fkooman@tuxed.net>
2012-04-05 20:24:28 +0200

    cleanup LDAP plugin, make it more safe

--- docs/LdapTest.php
+++ docs/LdapTest.php
@@ -1,11 +1,21 @@
 <?php
+
+if($argc < 2) {
+	echo "specify userId as a parameter" . PHP_EOL;
+	exit(1);
+}
+
 require_once 'lib/Voot/Provider.php';
 $config = parse_ini_file("config" . DIRECTORY_SEPARATOR . "voot.ini", TRUE);
 
 $vootStorageBackend = $config['voot']['storageBackend'];
 require_once "lib/Voot/$vootStorageBackend.php";
-$vootStorage = new $vootStorageBackend($config[$vootStorageBackend]);
 
-var_dump($vootStorage->isMemberOf($argv[1]));
-
+try { 
+	$vootStorage = new $vootStorageBackend($config[$vootStorageBackend]);
+	$memberShip = $vootStorage->isMemberOf($argv[1]);
+	var_dump($memberShip);
+} catch (Exception $e) { 
+	echo $e->getMessage() . PHP_EOL;
+}
 ?>
--- lib/Voot/LdapVootStorage.php
+++ lib/Voot/LdapVootStorage.php
@@ -7,15 +7,13 @@ class LdapVootStorage implements IVootStorage {
 
     public function __construct(array $config) {
         $this->_config = $config;
-        $this->_ldapConnection = ldap_connect($this->_config['uri']);
-
+        $this->_ldapConnection = @ldap_connect($this->_config['uri']);
         if(FALSE === $this->_ldapConnection) {
             throw new Exception("unable to connect to ldap server");
         }
-
         if(array_key_exists('bindDn', $this->_config) && array_key_exists('bindPass', $this->_config)) {
-            if(FALSE === ldap_bind($this->_ldapConnection, $this->_config['bindDn'], $this->_config['bindPass'])) {
-                throw new Exception("unable to bind to ldap server");
+            if(FALSE === @ldap_bind($this->_ldapConnection, $this->_config['bindDn'], $this->_config['bindPass'])) {
+                throw new Exception("unable to bind to ldap server, possibly invalid credentials");
             }
         }
     }
@@ -25,31 +23,56 @@ class LdapVootStorage implements IVootStorage {
     }
 
     public function isMemberOf($resourceOwnerId, $startIndex = null, $count = null) {
-        /* get the user DN */
+        $userGroups = array();
+
+        /* get the user distinguishedName */
         $filter = '(' . $this->_config['userIdAttribute'] . '=' . $resourceOwnerId . ')';
         $query = ldap_search($this->_ldapConnection, $this->_config['peopleDn'], $filter);
-        $entryID = ldap_first_entry($this->_ldapConnection, $query);
-        $userDn = ldap_get_dn($this->_ldapConnection, $entryID);
+        if(FALSE === $query) {
+            throw new Exception("directory query for user failed");
+        }
+        /* we assume there is only one entry for the specified user, if not
+           we only look at the first result */
+        $entry = ldap_first_entry($this->_ldapConnection, $query);
+        if(FALSE === $entry) {
+            throw new Exception("user not found in directory");
+        }
+        $userDn = ldap_get_dn($this->_ldapConnection, $entry);
+        if(FALSE === $userDn) {
+            throw new Exception("unable to get user distinguishedName");
+        }
 
-        /* get the group memberships */
+        /* get the groups the user is a member of */
         $filter = '(' . $this->_config['memberAttribute'] . '=' . $userDn . ')';
         $query = ldap_search($this->_ldapConnection, $this->_config['groupDn'], $filter);
+        if(FALSE === $query) {
+            throw new Exception("directory query for groups failed");
+        }
 
-        $groups = array();
-
-        for ($entryID = ldap_first_entry($this->_ldapConnection, $query); $entryID !== FALSE; $entryID = ldap_next_entry($this->_ldapConnection, $entryID)) {
-
-            $attributes = ldap_get_attributes($this->_ldapConnection, $entryID);
-
+        $entry = ldap_first_entry($this->_ldapConnection, $query);
+        while(FALSE !== $entry) {
+            $attributes = ldap_get_attributes($this->_ldapConnection, $entry);
+            if(FALSE === $attributes) {
+                throw new Exception("unable to get group attributes");
+            }
             $commonName = array_key_exists("cn", $attributes) ? $attributes["cn"][0] : "";
             $description = array_key_exists("description", $attributes) ? $attributes["description"][0] : "";
-            $distinguishedName = ldap_get_dn($this->_ldapConnection, $entryID);
+            $distinguishedName = ldap_get_dn($this->_ldapConnection, $entry);
+            if(FALSE === $distinguishedName) {
+                throw new Exception("unable to get distinguishedName");
+            }
+            array_push($userGroups, array ('id' => urlencode($distinguishedName),
+                                           'title' => $commonName, 
+                                           'description' => $description, 
+                                           'voot_membership_role' => 'member'));            
+            $entry = ldap_next_entry($this->_ldapConnection, $entry);
 
-            array_push($groups, array ( 'id' => urlencode($distinguishedName), 'title' => $commonName, 'description' => $description, 'voot_membership_role' => 'member'));
         }
+
+        // FIXME: we need to implement paging for LDAP as well...
         $startIndex = 0;
-        $totalResults = sizeof($groups);
-        return array ( 'startIndex' => $startIndex, 'totalResults' => $totalResults, 'itemsPerPage' => sizeof($groups), 'entry' => $groups);
+        $totalResults = sizeof($userGroups);
+        return array ( 'startIndex' => $startIndex, 'totalResults' => $totalResults, 'itemsPerPage' => $totalResults, 'entry' => $userGroups);
     }
 }
 
