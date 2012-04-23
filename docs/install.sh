4217c702d880618c7ee63eba57cb6f5ca8c65d1f
François Kooman <fkooman@tuxed.net>
2012-04-23 21:58:35 +0200

    pimp the OAuth management interface

--- docs/install.sh
+++ docs/install.sh
@@ -59,3 +59,17 @@ then
     wget -O ext/js/jso.js https://raw.github.com/fkooman/jso/master/jso.js
 fi
 
+if [ ! -d ext/bootstrap ]
+then
+	cd ext/
+	wget -O bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
+	unzip -o -q bootstrap.zip
+	rm bootstrap.zip
+    cd ../
+fi
+
+if [ ! -f ext/js/bootstrap-modal.js ]
+then
+    wget -O ext/js/bootstrap-modal.js http://twitter.github.com/bootstrap/assets/js/bootstrap-modal.js
+fi
+
--- manage/manage.js
+++ manage/manage.js
@@ -1,111 +1,149 @@
 $(document).ready(function () {
+
+    var apiRoot = 'http://localhost/voot';
+
     jso_configure({
         "manage": {
             client_id: "manage",
-            redirect_uri: "http://localhost/voot/manage/index.html",
-            authorization: "http://localhost/voot/oauth/authorize"
+            redirect_uri: apiRoot + "/manage/index.html",
+            authorization: apiRoot + "/oauth/authorize"
         }
     });
     jso_ensureTokens({
         "manage": ["read"]
     });
 
-    function loadClients() {
+    function renderClientList() {
         $.oajax({
-            url: "http://localhost/voot/oauth/client",
+            url: apiRoot + "/oauth/client",
             jso_provider: "manage",
             jso_scopes: ["read"],
             jso_allowia: true,
             dataType: 'json',
             success: function (data) {
-                if (data) {
-                    $("#clientList").html(
-                    $("#clientListTemplate").render(data));
-                    addClickHandlers();
-                } else {
-                    alert("An error occurred.");
-                }
+                $("#clientList").html($("#clientListTemplate").render(data));
+                addClientListHandlers();
             }
         });
     }
 
-    function addClickHandlers() {
-        $("a.delete").click(function () {
-            var clientId = $(this).attr('href').slice(1);
-            //var clientName = $(this).parent().prev().children("a").html();
-            if (confirm("Are you sure you want to delete this client?")) {
-                $.oajax({
-                    url: "http://localhost/voot/oauth/client/" + clientId,
-                    jso_provider: "manage",
-                    jso_scopes: ["read"],
-                    jso_allowia: true,
-                    type: "DELETE",
-                    success: function (data) {
-                        loadClients();
-                    }
-                });
-            }
+    function addClientListHandlers() {
+        $("a.editClient").click(function () {
+            editClient($(this).data('clientId'));
         });
-        $("a.edit").click(function () {
-            var clientId = $(this).attr('href').slice(1);
-            $.oajax({
-                url: "http://localhost/voot/oauth/client/" + clientId,
-                jso_provider: "manage",
-                jso_scopes: ["read"],
-                jso_allowia: true,
-                success: function (data) {
-                    $("div.editForm").html($("#clientEditTemplate").render(data));
-                    $("div.editClient").show();
-                    $('form').submit(function () {
-                        var params = {};
-                        jQuery.each(jQuery('form').serializeArray(), function (k, v) {
-                            params[v.name] = (v.value === '') ? null : v.value;
-                        });
-                        x = JSON.stringify(params);
-                        $.oajax({
-                            url: "http://localhost/voot/oauth/client/" + clientId,
-                            jso_provider: "manage",
-                            jso_scopes: ["read"],
-                            jso_allowia: true,
-                            type: "PUT",
-                            dataType: 'json',
-                            data: x,
-                            success: function (data) {
-                                $("div.editClient").hide();
-                                loadClients();
-                            }
-                        });
-                        return false;
-                    });
-                }
-            });
+        $("a.deleteClient").click(function() {
+            if(confirm("Are you sure you want to delete '" + $(this).data('clientName') + "'")) {
+               deleteClient($(this).data('clientId'));
+            }
         });
     }
 
-    function addOtherClickHandlers() {}
-    $("button#addClient").click(function () {
-        $("div.addForm").html($("#clientEditTemplate").render({}));
-        $("div.addClient").show();
-        $('form').submit(function () {
-            var params = {};
-            jQuery.each(jQuery('form').serializeArray(), function (k, v) {
-                params[v.name] = (v.value === '') ? null : v.value;
-            });
-            x = JSON.stringify(params);
+    function deleteClient(clientId) { 
+        $.oajax({
+            url: apiRoot + "/oauth/client/" + clientId,
+            jso_provider: "manage",
+            jso_scopes: ["read"],
+            jso_allowia: true,
+            type: "DELETE",
+            success: function (data) {
+                renderClientList();
+            }
+        });
+    }
+
+    function editClient(clientId) {
+        if(clientId) {
+            // client specified, we edit
             $.oajax({
-                url: "http://localhost/voot/oauth/client",
+                url: apiRoot + "/oauth/client/" + clientId,
                 jso_provider: "manage",
                 jso_scopes: ["read"],
                 jso_allowia: true,
-                type: "POST",
-                dataType: 'json',
-                data: x,
                 success: function (data) {
-                    loadClients();
+                    $("#editModal").html($("#clientEditTemplate").render(data));
+                    $("#editModal").show();
+                    addEditClientHandlers();
                 }
             });
-            return false;
+        } else {
+            // no client specified, we add
+            var data = {};
+            $("#editModal").html($("#clientEditTemplate").render(data));
+            $("#editModal").show();
+            addEditClientHandlers();
+        }
+    }
+    
+    function parseForm(formData) {
+        var params = {};
+        $.each(formData.serializeArray(), function (k, v) {
+            params[v.name] = (v.value === '') ? null : v.value;
         });
+        return JSON.stringify(params);
+    }
+   
+    function addEditClientHandlers() {
+        $("#editModal a.close").click(function () {
+            $("#editModal").hide();
+        });
+        $("#editModal a.editClose").click(function () {
+            $("#editModal").hide();
+        });
+        $("#editModal a.editSave").click(function () {
+            // FIXME: not really a nice way to fetch form data...
+            var clientData = parseForm($('form.editClient'));
+            if($(this).data('clientId')) {
+                // if clientId was available, we update
+                updateClient($(this).data('clientId'), clientData);
+            } else {
+                // or add...
+                addClient(clientData);
+            }
+        });
+    }
+
+    function updateClient(clientId, clientData) {
+        $.oajax({
+            url: apiRoot + "/oauth/client/" + clientId,
+            jso_provider: "manage",
+            jso_scopes: ["read"],
+            jso_allowia: true,
+            type: "PUT",
+            dataType: 'json',
+            data: clientData,
+            success: function (data) {
+                $("#editModal").hide();
+                renderClientList();
+            }
+        });
+    }
+
+    function addClient(clientData) {
+        $.oajax({
+            url: apiRoot + "/oauth/client",
+            jso_provider: "manage",
+            jso_scopes: ["read"],
+            jso_allowia: true,
+            type: "POST",
+            dataType: 'json',
+            data: clientData,
+            success: function (data) {
+                $("#editModal").hide();
+                renderClientList();
+            }
+        });
+    }
+
+    $("a.addClient").click(function () {
+        editClient();
     });
-    loadClients();
+
+
+    function initPage() {
+        $("#editModal").hide();
+        renderClientList();
+    }
+
+    initPage();
+
 });
