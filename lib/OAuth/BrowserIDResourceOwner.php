6802e5f402ace3ee78667348387d8de31edb0a62
François Kooman <fkooman@tuxed.net>
2012-05-15 16:02:44 +0200

    update BrowserID, still not working

--- lib/OAuth/BrowserIDResourceOwner.php
+++ lib/OAuth/BrowserIDResourceOwner.php
@@ -4,23 +4,24 @@ require_once 'ext/browserid-session/browserid.php';
 
 class BrowserIDResourceOwner implements IResourceOwner {
 
-    private $_config, $_verifier;
+    private $_config;
+    private $_verifier;
 
-    public function __construct(array $config) {
-        $this->_config = $config;
-        $this->_verifier = new BrowserIDVerifier($config);
+    public function __construct(Config $c) {
+        $this->_c = $c;
+        $this->_verifier = new BrowserIDVerifier();
     }
 
     public function getResourceOwnerId() {
         $this->_verifier->requireAuth();
         $attributes = $this->_verifier->getAttributes();
-        return $attributes[$this->_config['resourceOwnerIdAttributeName']];
+        return $attributes[$this->_c->getSectionValue('BrowserIDResourceOwner', 'resourceOwnerIdAttributeName')];
     }
 
     public function getResourceOwnerDisplayName() {
         $this->_verifier->requireAuth();
         $attributes = $this->_verifier->getAttributes();
-        return $attributes[$this->_config['resourceOwnerDisplayNameAttributeName']];
+        return $attributes[$this->_c->getSectionValue('BrowserIDResourceOwner', 'resourceOwnerDisplayNameAttributeName')];
     }
 
 }
