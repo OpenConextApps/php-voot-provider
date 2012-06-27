d6b8d001cfd2fd8b1f16aebd7dc061aec4461adb
François Kooman <fkooman@tuxed.net>
2012-06-27 14:41:43 +0200

    make password configurable

--- index.php
+++ index.php
@@ -9,14 +9,17 @@ $app = new Slim(array(
     // we need to disable Slim's session handling due to incompatibilies with
     // simpleSAMLphp sessions
     'session.handler' => null,
-    //'mode' => 'production',
-    'mode' => 'development',
-    'debug' => true,
+    'mode' => 'production',
+    //'mode' => 'development',
+    'debug' => false,
     'log.writer' => new TimestampLogFileWriter(array('path' => 'data' . DIRECTORY_SEPARATOR . 'logs')),
 ));
-$app->add(new HttpBasicAuth('theUsername', 'thePassword'));
 
 $vootConfig = new Config(__DIR__ . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "voot.ini");
+
+$authBackend = $vootConfig->getValue('authBackend');
+// FIXME: just give all configuration parameters (vootConfig->getSection(...)) to the constructor
+$app->add(new $authBackend($vootConfig->getSectionValue('HttpBasicAuth','username'), $vootConfig->getSectionValue('HttpBasicAuth','password')));
 
 // VOOT
 $t = new SlimVoot($app, $vootConfig);
