09e8b93e32f03381fc643f74ad1f54d497665399
François Kooman <fkooman@tuxed.net>
2012-06-27 11:24:42 +0200

    remove all (demo) client stuff

--- docs/install_dependencies.sh
+++ docs/install_dependencies.sh
@@ -22,29 +22,3 @@ git clone git://github.com/codeguy/Slim-Extras.git
 cd Slim-Extras/
 git checkout ${SLIM_EXTRAS_VERSION}
 cd ../../
-
-# jQuery
-wget -O ext/js/jquery.js http://code.jquery.com/jquery.min.js
-
-# JSrender (JavaScript Template Rendering for jQuery)
-wget -O ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsrender.js
-
-# JSO (JavaScript OAuth 2 client)
-wget -O ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
-
-# Bootstrap
-cd ext/
-wget -O bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
-unzip -o -q bootstrap.zip
-rm bootstrap.zip
-cd ../
-
-# Bootstrap Modal
-wget -O ext/js/bootstrap-modal.js http://twitter.github.com/bootstrap/assets/js/bootstrap-modal.js
-
-# Bootstrap Tooltip
-wget -O ext/js/bootstrap-tooltip.js http://twitter.github.com/bootstrap/assets/js/bootstrap-tooltip.js
-
-# Bootstrap Popover
-wget -O ext/js/bootstrap-popover.js http://twitter.github.com/bootstrap/assets/js/bootstrap-popover.js
-
